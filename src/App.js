ef751c47e9e0a2622122c65cd82f3ccbcf4371e3
Motez Farwa <Farwamotez@gmail.com>
2025-03-05 14:56:37 +0100

    paiement and create  course for instructor

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import ProtectedRoute from './pages/ProtectedRoute';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Template from './pages/template';
+import Addcourseform from './pages/Addcourseform';
+import CourseList from './pages/CourseList';
+import Coursedetails from './pages/Coursedetails';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import PaymentForm from './pages/Paiement';
+
+
+// Your Stripe publishable key (replace with your own key)
+const stripePromise = loadStripe('pk_test_51PFIvmRsp6m9X8kfwMev0UG3kepv04iTwrVtDodndQOMD4YspYgN424hpZ8i36gwb0CMldjLvS8gtS2YeguA21Cb00twxbZdDL');
 
 function App() {
   return (
@@ -24,7 +34,20 @@ function App() {
   {/* Protected Routes - Role Based Access */}
   <Route path="/home" element={<ProtectedRoute allowedRoles={['INSTRUCTEUR']}><Template /></ProtectedRoute>} />
   <Route path="/dashboard" element={<ProtectedRoute allowedRoles={['ADMIN']}><Dashboard /></ProtectedRoute>} />
-  
+  <Route path="/Ajoutercour" element={<ProtectedRoute allowedRoles={['ADMIN']}><Addcourseform /></ProtectedRoute>} />
+  <Route path="/getcours" element={<ProtectedRoute allowedRoles={['ADMIN', 'INSTRUCTEUR']}><CourseList /></ProtectedRoute>} />
+  <Route path="/cours/:id" element={<ProtectedRoute allowedRoles={['ADMIN', 'INSTRUCTEUR']}><Coursedetails /></ProtectedRoute>} />
+  <Route
+  path="/payment"
+  element={
+    <ProtectedRoute allowedRoles={['ADMIN', 'INSTRUCTEUR']}>
+      <Elements stripe={stripePromise}>
+        <PaymentForm />
+      </Elements>
+    </ProtectedRoute>
+  }
+/>
+
   </Routes>
   </Router>
   </div>
